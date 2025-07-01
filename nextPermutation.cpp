206b7fc7d57834f6db4b6d2b1b016cfb0654f9ca
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2025-07-01 15:31:25 +0530

    Create nextPermutation.cpp

---
+++ nextPermutation.cpp
@@ -0,0 +1,39 @@
+// next permutation
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+
+        int ind=-1; //index
+
+        //step 1 : find the breaking point where a<a+1
+
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
+            }
+            
+        }
+
+        if(ind==-1){
+             reverse(nums.begin(),nums.end());
+             return;
+             
+        }
+
+        //step 2 : swap it with the next greater element
+        for(int i=n-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+
+        //step 3: reverse the remaining elements after the index
+      
+            reverse(nums.begin() + ind+1 , nums.end());
+
+    }
+};
